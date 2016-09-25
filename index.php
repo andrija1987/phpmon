e960cba1fa1da7c5e2693c4a9a427bd38702d5c9
Andrija Barbarosa <andrija.barbarosa@gmail.com>
2016-09-26 01:09:44 +0200

    openshift changes

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 global $sJavascript, $sTable;
-$con = mysqli_connect("localhost","database","database","database");
+$con=mysqli_connect("localhost","root","Lokarda7","phpmon");
 $query=mysqli_query($con, "SELECT * FROM servers ORDER BY id") or die(mysqli_error());
 	$sJavascript .= '<script type="text/javascript">
 		function uptime() {
@@ -57,10 +57,10 @@ include ($index);
 
 ?>
 
-<!--<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>-->
+<script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
 
 
 
